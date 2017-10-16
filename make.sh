3b655c27f9fcfd1d8a66a500ce1690d76b053610
PaddyWan <addybombeke@gmail.com>
2017-10-16 14:09:13 +0200

    Update make.sh

--- make.sh
+++ make.sh
@@ -7,4 +7,4 @@ gcc -c -O0 -g3 -fno-inline -DDEBUG -std=gnu++14 -fdiagnostics-color=always -Wall
 gcc -c -O0 -g3 -fno-inline -DDEBUG -std=gnu++14 -fdiagnostics-color=always -Wall -fconcepts src/std.cpp -o obj/std-d.opp 
 gcc -c -O0 -g3 -fno-inline -DDEBUG -std=gnu++14 -fdiagnostics-color=always -Wall -fconcepts src/iostream/stdutil.cpp -o obj/iostream/stdutil-d.opp 
 gcc -c -O0 -g3 -fno-inline -DDEBUG -std=gnu++14 -fdiagnostics-color=always -Wall -fconcepts src/main.cpp -o obj/main-d.opp
-g++ -lstdc++ -lm -lbz2 -lz -llzma -lcrypto obj/internal/string-d.opp obj/iostream/stdutil-d.opp obj/main-d.opp obj/std-d.opp /home/werkmap/lib/bin/lib-d.app -o bin/stream-d.epp
+g++ -lstdc++ -lm -lbz2 -lz -llzma -lcrypto obj/internal/string-d.opp obj/iostream/stdutil-d.opp obj/main-d.opp obj/std-d.opp -o bin/stream-d.epp
