9bcfa06d095e851091b6c02cb6b0c0f48253c880
PaddyWan <addybombeke@gmail.com>
2017-10-16 14:03:53 +0200

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 mkdir obj
 mkdir obj/internal
 mkdir obj/iostream
 mkdir bin
-gcc -c -O0 -g3 -fno-inline -DDEBUG -std=gnu++14 -fdiagnostics-color=always -Wall -fconcepts -DNUMBER_OF_THREADS=12 -I/home/werkmap/lib/src src/internal/string.cpp -o obj/internal/string-d.opp 
-gcc -c -O0 -g3 -fno-inline -DDEBUG -std=gnu++14 -fdiagnostics-color=always -Wall -fconcepts -DNUMBER_OF_THREADS=12 -I/home/werkmap/lib/src src/std.cpp -o obj/std-d.opp 
-gcc -c -O0 -g3 -fno-inline -DDEBUG -std=gnu++14 -fdiagnostics-color=always -Wall -fconcepts -DNUMBER_OF_THREADS=12 -I/home/werkmap/lib/src src/iostream/stdutil.cpp -o obj/iostream/stdutil-d.opp 
-gcc -c -O0 -g3 -fno-inline -DDEBUG -std=gnu++14 -fdiagnostics-color=always -Wall -fconcepts -DNUMBER_OF_THREADS=12 -I/home/werkmap/lib/src src/main.cpp -o obj/main-d.opp
+gcc -c -O0 -g3 -fno-inline -DDEBUG -std=gnu++14 -fdiagnostics-color=always -Wall -fconcepts src/internal/string.cpp -o obj/internal/string-d.opp 
+gcc -c -O0 -g3 -fno-inline -DDEBUG -std=gnu++14 -fdiagnostics-color=always -Wall -fconcepts src/std.cpp -o obj/std-d.opp 
+gcc -c -O0 -g3 -fno-inline -DDEBUG -std=gnu++14 -fdiagnostics-color=always -Wall -fconcepts src/iostream/stdutil.cpp -o obj/iostream/stdutil-d.opp 
+gcc -c -O0 -g3 -fno-inline -DDEBUG -std=gnu++14 -fdiagnostics-color=always -Wall -fconcepts src/main.cpp -o obj/main-d.opp
 g++ -lstdc++ -lm -lbz2 -lz -llzma -lcrypto obj/internal/string-d.opp obj/iostream/stdutil-d.opp obj/main-d.opp obj/std-d.opp /home/werkmap/lib/bin/lib-d.app -o bin/stream-d.epp
